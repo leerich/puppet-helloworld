25705e1d5b9bea3be5556ec22c262b4c0b38e36a
Lee Rich <aaa@aaa.com>
2015-05-08 13:51:13 -0400

    motd change

--- manifests/motd.pp
+++ manifests/motd.pp
@@ -4,7 +4,7 @@ class helloworld::motd {
     owner  => 'root',
     group  => 'root',
     mode    => '0644',
-    content => "hello, world! Welcome to Puppet\n",
+    content => "hello, world! Welcome to Puppet and more!\n",
     }
 
  }
